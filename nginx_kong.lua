2677d2793026d966f20b3756b13c6f8a35eb9d04
Vasily Angapov <angapov@gmail.com>
2018-09-11 19:13:07 +0530

    Bump large_client_header_buffers

--- nginx_kong.lua
+++ nginx_kong.lua
@@ -73,8 +73,7 @@ server {
     listen 443 ssl;
     server_name kong;
     set $session_secret '';
-    client_header_buffer_size 16k;
-    large_client_header_buffers 16 64k;
+    large_client_header_buffers 16 256k;
     error_page 400 404 408 411 412 413 414 417 494 /kong_error_handler;
     error_page 500 502 503 504 /kong_error_handler;
 
